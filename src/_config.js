aa78d6aa5f637c6c6382b04e0408683d8ea67521
ytimocin <ytimocin@gmail.com>
2016-10-06 00:46:17 -0700

    changes

--- src/_config.js
+++ src/_config.js
@@ -2,9 +2,10 @@ var config = {};
 
 // mongo uri
 config.mongoURI = {
-  development: "mongodb://yeko:12345@ds013926.mlab.com:13926/heroku_9k5sszl0",
-  test: "mongodb://yeko:12345@ds013926.mlab.com:13926/heroku_9k5sszl0",
-  stage: process.env.MONGOLAB_URI
+    development: "mongodb://yeko:12345@ds061325.mlab.com:61325/heroku_w8m2tknk",
+    test: "mongodb://yeko:12345@ds061325.mlab.com:61325/heroku_w8m2tknk",
+    stage: "mongodb://yeko:12345@ds061325.mlab.com:61325/heroku_w8m2tknk",
+    production: "mongodb://yeko:12345@ds061325.mlab.com:61325/heroku_w8m2tknk"
 };
 
 module.exports = config;
